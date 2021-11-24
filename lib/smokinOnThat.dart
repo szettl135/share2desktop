40fd1b5add6e3a57710daefc1a459b8606cfe746
jtrebicki <54892353+jtrebicki@users.noreply.github.com>
2021-11-24 18:54:07 +0100

    gute beispiel njia´´oah

--- lib/smokinOnThat.dart
+++ lib/smokinOnThat.dart
@@ -7,20 +7,27 @@ class Programm extends StatelessWidget {
         appBar: new AppBar(title: new Text("Seas")),
         body: Column(
           children: [
-            
+          //Einstellungen
             Row(
               children: [
                 Spacer(flex: 1),
-                 Container(alignment: Alignment.topRight, 
+                 Container(padding: EdgeInsets.all(10),
+                 alignment: Alignment.topRight, 
                 child: TextButton(child: Row(children: [
                 Text("Einstellungen"),
+                SizedBox(width: 2),
                 Icon(Icons.settings)
               ]),            
-            onPressed: null,
+            onPressed: () => {
+  print("lmao")
+},
             ))
               ],
-            )
-           
+            ),
+          
+          SizedBox(height: 20),
+          //Logo und Titel
+          Text("sample text")
           ],
 
         )
