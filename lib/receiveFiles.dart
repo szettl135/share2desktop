9411bb2cf74274f389d9ee958f5da66ae7a1767e
jtrebicki <54892353+jtrebicki@users.noreply.github.com>
2021-12-04 12:01:11 +0100

    listtile beim annehmen
    viel besser

--- lib/receiveFiles.dart
+++ lib/receiveFiles.dart
@@ -13,36 +13,76 @@ class _ReceiveFiles extends State<ReceiveFiles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(title: new Text("Share2Desktop")),
-      body: Column(children: [
-        Container(padding: EdgeInsets.all(30), child:  Text("Dateien empfangen",style: Theme.of(context).textTheme.headline2)),
-        SizedBox(height: 50),
-        Row(mainAxisAlignment:MainAxisAlignment.center ,children: [
-          OutlinedButton(child: Text("Alles annehmen"), onPressed: () => {print("alles annehmen")}),SizedBox(width: 30),OutlinedButton(child: Text("Alles ablehnen"), onPressed: () => {print("ablehnen")})
-        ],),
-        Spacer(flex: 2),
-        Container(
-
+        appBar: new AppBar(title: new Text("Share2Desktop")),
+        body: Column(children: [
+          Container(
+              padding: EdgeInsets.all(30),
+              child: Text("Dateien empfangen",
+                  style: Theme.of(context).textTheme.headline2)),
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                  child: Text("Alles annehmen"),
+                  onPressed: () => {print("alles annehmen")}),
+              SizedBox(width: 30),
+              OutlinedButton(
+                  child: Text("Alles ablehnen"),
+                  onPressed: () => {print("ablehnen")})
+            ],
+          ),
+          Spacer(flex: 2),
+          Container(
               padding: EdgeInsets.all(50),
               height: MediaQuery.of(context).size.height * 0.5,
-              child: ListView(
-                padding: const EdgeInsets.all(8),
-                children: <Widget>[
-                  //TextButton(child: Text("video.mp4"),onPressed: () => (print("Video.mp4")),)
+              child:
+                  ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+                //TextButton(child: Text("video.mp4"),onPressed: () => (print("Video.mp4")),)
 
-                  //ein einzelnes Element
-                  Row(mainAxisAlignment:MainAxisAlignment.center,children: [
-                    Text("video.mp4"),SizedBox(width: 50),IconButton(onPressed: () => {print("angenommen")}, icon: const Icon(Icons.save_alt)),SizedBox(width: 50),IconButton(onPressed: () => {print("abgelehnt")}, icon: const Icon(Icons.close))
-                  ],),
+                //ein einzelnes Element
+                /*Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("video.mp4"),
+                    SizedBox(width: 50),
+                    IconButton(
+                        onPressed: () => {print("angenommen")},
+                        icon: const Icon(Icons.save_alt)),
+                    SizedBox(width: 50),
+                    IconButton(
+                        onPressed: () => {print("abgelehnt")},
+                        icon: const Icon(Icons.close))
+                  ],
+                ),*/
 
-                  Row(mainAxisAlignment:MainAxisAlignment.center,children: [
-                    Text("archiv.zip"),SizedBox(width: 50),IconButton(onPressed: () => {print("angenommen")}, icon: const Icon(Icons.save_alt)),SizedBox(width: 50),IconButton(onPressed: () => {print("abgelehnt")}, icon: const Icon(Icons.close))
-                  ],),
-            
-                ])),
-        Spacer(flex:1)
-       
-      ])
-    );
+                ListTile(
+                  title: Text('urlaub.mp4'),
+                  subtitle: Text('Video | 10 MB'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(onPressed: ()=> {print("annehmen")}, icon: Icon(Icons.save_alt)),
+                      SizedBox(width: 30),
+                      IconButton(onPressed: ()=> {print("ablehnen")}, icon: Icon(Icons.close)),
+                    ],
+                  ),
+                ),
+                //wir verwenden MB statt das richtige MiBi, um Leute nicht zu verwirren (Windows Standard)
+                ListTile(
+                  title: Text('landschaft.jpg'),
+                  subtitle: Text('Bild | 1 MB'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(onPressed: ()=> {print("annehmen")}, icon: Icon(Icons.save_alt)),
+                      SizedBox(width: 30),
+                      IconButton(onPressed: ()=> {print("ablehnen")}, icon: Icon(Icons.close)),
+                    ],
+                  ),
+                )
+              ])),
+          Spacer(flex: 1)
+        ]));
   }
 }
