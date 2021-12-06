1ecbe31b75e129d2c1c71ee3ec57fd799d27dd6e
jtrebicki <54892353+jtrebicki@users.noreply.github.com>
2021-12-06 13:26:46 +0100

    unnötiges weg

--- lib/chooseFiles.dart
+++ lib/chooseFiles.dart
@@ -3,14 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:share2desktop/receiveFiles.dart';
 
 
-import 'dart:async';
-import 'dart:developer' as developer;
-import 'dart:io';
-
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class ChooseFiles extends StatefulWidget {
   final String targetDeviceName;
