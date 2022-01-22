a739028c432541a0d064827022c06e7f3482e1c2
jtrebicki <54892353+jtrebicki@users.noreply.github.com>
2022-01-22 11:09:19 +0100

    ALLES FUNKTIONIERT OMG
    nach 9 Jahren in Development hoffentlich war es worth den wait

--- lib/anleitung.dart
+++ lib/anleitung.dart
@@ -58,130 +58,129 @@ class _Anleitung extends State<Anleitung> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(title: new Text("Share2Desktop")),
-        body: Container(
-            padding: EdgeInsets.all(15.0),
-            child: Column(children: [
-              Container(
-                child: ScrollConfiguration(
-                    behavior: AppScrollBehavior(),
-                    child: Expanded(child: PageView(
-                      
-                      controller: controller,
-                      onPageChanged: (page) => {_current=page},
-                      pageSnapping: true,
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        Container(
-                            padding: EdgeInsets.all(15.0),
-                            color: Colors.cyan,
-                            child: Center(
-                                child: Column(children: [
-                              Spacer(),
-                              Text(
-                                AppLocalizations.of(context)!
-                                    .instructions1_header,
-                                style: TextStyle(
-                                    fontSize: 26,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                AppLocalizations.of(context)!.instructions1,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              FittedBox(
-                                  fit: BoxFit.fill,
-                                  child: Image.asset(
-                                    'assets/share2desktop1.png',
-                                    height: MediaQuery.of(context).size.height *
-                                        0.2,
-                                  )),
-                              Spacer()
-                            ]))),
-                        Container(
-                            padding: EdgeInsets.all(15.0),
-                            color: Colors.green,
-                            child: Center(
-                                child: Column(children: [
-                                  Spacer(),
-                              Text(
-                                AppLocalizations.of(context)!
-                                    .instructions2_header,
-                                style: TextStyle(
-                                    fontSize: 26,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                AppLocalizations.of(context)!.instructions2,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Spacer()
-                                ] ))),
-                        Container(
-                            padding: EdgeInsets.all(15.0),
-                            color: Colors.purple,
-                            child: Center(
-                                child: Column(children: [
-                                  Spacer(),
-                              Text(
-                                AppLocalizations.of(context)!
-                                    .instructions3_header,
-                                style: TextStyle(
-                                    fontSize: 26,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                AppLocalizations.of(context)!.instructions3,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              
-                              Spacer()
-                                ]),
-                      
-                    )),
-                    Container(
-                            padding: EdgeInsets.all(15.0),
-                            color: Colors.orange,
-                            child: Center(
-                                child: Column(children: [
-                                  Spacer(),
-                              Text(
-                                AppLocalizations.of(context)!
-                                    .instructions4_header,
-                                style: TextStyle(
-                                    fontSize: 26,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                AppLocalizations.of(context)!.instructions4,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                               Spacer()
-                                ]),
-                      
-                    )),
-          
-                      ]))
-        )),
+        body: Column(children: [
+              Expanded(
+                flex: 5,
+                child: PageView(
+                        
+                        controller: controller,
+                        onPageChanged: (page) => {_current=page},
+                        pageSnapping: true,
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          Container(
+                             
+                              padding: EdgeInsets.all(20.0),
+                              color: Colors.cyan,
+                              child: Center(
+                                  child: Column(children: [
+                                Spacer(),
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .instructions1_header,
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  AppLocalizations.of(context)!.instructions1,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Container(
+                                    
+                                    child: Image.asset(
+                                      'assets/share2desktop1.png',
+                                      height: MediaQuery.of(context).size.height *
+                                          0.2,
+                                    fit: BoxFit.contain,)),
+                                Spacer()
+                              ]))),
+                          Container(
+                              padding: EdgeInsets.all(15.0),
+                              color: Colors.green,
+                              child: Center(
+                                  child: Column(children: [
+                                    Spacer(),
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .instructions2_header,
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  AppLocalizations.of(context)!.instructions2,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Spacer()
+                                  ] ))),
+                          Container(
+                              padding: EdgeInsets.all(15.0),
+                              color: Colors.purple,
+                              child: Center(
+                                  child: Column(children: [
+                                    Spacer(),
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .instructions3_header,
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  AppLocalizations.of(context)!.instructions3,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                
+                                Spacer()
+                                  ]),
+                        
+                      )),
+                      Container(
+                              padding: EdgeInsets.all(15.0),
+                              color: Colors.orange,
+                              child: Center(
+                                  child: Column(children: [
+                                    Spacer(),
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .instructions4_header,
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  AppLocalizations.of(context)!.instructions4,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                 Spacer()
+                                  ]),
+                        
+                      )),
+                        
+                        ]),
+              ),
+        
         SizedBox(height: MediaQuery.of(context).size.width * 0.01),
         Center(child: 
         SmoothPageIndicator(  
@@ -192,8 +191,9 @@ class _Anleitung extends State<Anleitung> {
          controller.jumpToPage(index);
 
    }  
-)  )
-        ])));
+)  ),
+SizedBox(height: MediaQuery.of(context).size.width * 0.01),
+        ]));
   }
 }
      
